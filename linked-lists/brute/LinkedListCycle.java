a923d1da5d8cc71fa02f98e5f87c7495c2b34fba
ifrabilal003 <ifrabilal003@gmail.com>
2026-06-28 22:54:50 +0500

    feat(linked-lists): add brute force solution for Linked List Cycle

---
+++ linked-lists/brute/LinkedListCycle.java
@@ -0,0 +1,31 @@
+
+
+import java.util.*;
+
+public class LinkedListCycle {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) { this.val = val; }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> seen = new HashSet<>();
+        while (head != null) {
+            if (seen.contains(head)) return true;
+            seen.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LinkedListCycle sol = new LinkedListCycle();
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = head.next; 
+        System.out.println(sol.hasCycle(head)); 
+    }
+}
