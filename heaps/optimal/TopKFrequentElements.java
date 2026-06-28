a90fc571e6e03cd2c223abab4bc879a0b9412b6c
ifrabilal003 <ifrabilal003@gmail.com>
2026-06-28 23:01:54 +0500

    Create TopKFrequentElements.java

---
+++ heaps/optimal/TopKFrequentElements.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+public class TopKFrequentElements {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int n : nums) freq.put(n, freq.getOrDefault(n, 0) + 1);
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+            (a, b) -> freq.get(a) - freq.get(b));
+        for (int n : freq.keySet()) {
+            minHeap.offer(n);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) result[i] = minHeap.poll();
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TopKFrequentElements sol = new TopKFrequentElements();
+        System.out.println(Arrays.toString(
+            sol.topKFrequent(new int[]{1,1,1,2,2,3}, 2))); 
+    }
+}
