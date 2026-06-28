822fd3be5f400cd8e7989ed57ea91287a85b7e1b
ifrabilal003 <ifrabilal003@gmail.com>
2026-06-28 22:51:18 +0500

    feat(two-pointers): add optimal solution for Container With Most Water

---
+++ two-pointers/optimal/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxWater = 0;
+        while (left < right) {
+            int water = Math.min(height[left], height[right]) * (right - left);
+            maxWater = Math.max(maxWater, water);
+            if (height[left] < height[right]) left++;
+            else right--;
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        ContainerWithMostWater sol = new ContainerWithMostWater();
+        System.out.println(sol.maxArea(new int[]{1,8,6,2,5,4,8,3,7})); 
+    }
+}
